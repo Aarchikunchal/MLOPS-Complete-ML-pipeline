d8c0e8e71d8e9f5e995060f503cd5d3a0a56a44a
Aarchi <kunchalaarchi@gmail.com>
2025-11-13 11:42:11 +0530

    params configured for the pipelines

--- src/model_building.py
+++ src/model_building.py
@@ -29,6 +29,22 @@ logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
 
+def load_params(params_path: str) ->dict:
+    try:
+        with open(params_path,'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug("paramters retrieved: %s",params_path)
+        return params
+    except FileNotFoundError:
+        logger.debug("file not found: %s",params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.debug("yaml error: %s",e)
+    except Exception  as e:
+        logger.debug("unexpected error: %s",e) 
+
+
+
 
 
 def load_data(file_path: str) -> pd.DataFrame:
@@ -104,7 +120,8 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        params = {'n_estimators': 25,'random_state':2}
+        params = load_params('params.yaml')['model_building']
+        
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
